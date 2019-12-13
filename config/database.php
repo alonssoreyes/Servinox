ff29bb5812cb311620d0d7aa627219352c4d8299
alonssoreyes <53458620+alonssoreyes@users.noreply.github.com>
2019-12-13 14:44:46 -0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -4,8 +4,9 @@ class Database {
 
     public static function conectar() {
         $servidor = "sqlsrv:Server=localhost;Database=snx;";
-        $con = new PDO($servidor, "sa", "alonso");
+        $con = new PDO($servidor, "YOUR_USER", "YOUR_PASSWORD");
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //Setear codificacion de caracteres (Acentos etc)
         $con->setAttribute(PDO::SQLSRV_ATTR_ENCODING, PDO::SQLSRV_ENCODING_UTF8);
 
 
